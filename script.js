6ed96eb3c20d7d3e4b50af795879909ad513b3ae
Gautam Singh Badwal <140250464+GautamSinghBadwal@users.noreply.github.com>
2024-02-16 18:45:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Navbar #2 - Sticky Functioning */
+const navbar = document.querySelector(".airpods-navbar");
+const originalY = navbar.offsetTop; // 44
+
+console.log(document.querySelector(".section-01").offsetTop);
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= originalY) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
+
+let controller = new ScrollMagic.Controller();
